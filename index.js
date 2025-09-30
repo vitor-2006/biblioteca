73874f9961a808b16aeef56cf52ef48967ca9541
vitor-2006 <vitorbalbinotdellatorre@gmail.com>
2025-09-30 09:45:31 -0300

    adicionando segurança vtnc

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ import { routesBook } from './livros/routes.js';
 import { routesEstudante } from './estudantes/routes.js';
 import { routesAluguel } from './alugueis/routes.js';
 
+dotenv.config()
+const dbUser = process.env.DB_USER
+const dbPass = process.env.DB_PASSWORD
+
 const app = express();
 const port = 3000;
 mongoose.connect(
-    "mongodb+srv://vitor-2006:MongoDB7993@cluster0.nkow1bb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+    `mongodb+srv://${dbUser}:${dbPass}@cluster0.nkow1bb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
 )
 
 mongoose.connection.once("open", () => {
