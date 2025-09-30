3af6874e0437d8b7ab26f327530e53011cd752c3
vitor-2006 <vitorbalbinotdellatorre@gmail.com>
2025-09-30 10:09:11 -0300

    n vou usar isso agr

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
 import express from 'express'
-import { mongoose } from 'mongoose';
+// import { mongoose } from 'mongoose';
 import { routesBook } from './livros/routes.js';
 import { routesEstudante } from './estudantes/routes.js';
 import { routesAluguel } from './alugueis/routes.js';
 
-dotenv.config()
-const dbUser = process.env.DB_USER
-const dbPass = process.env.DB_PASSWORD
+// dotenv.config()
+// const dbUser = process.env.DB_USER
+// const dbPass = process.env.DB_PASSWORD
 
-const app = express();
-const port = 3000;
-mongoose.connect(
-    `mongodb+srv://${dbUser}:${dbPass}@cluster0.nkow1bb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-)
+// const app = express();
+// const port = 3000;
+// mongoose.connect(
+//     `mongodb+srv://${dbUser}:${dbPass}@cluster0.nkow1bb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
+// )
 
-mongoose.connection.once("open", () => {
-    console.log("Conectado ao mongoDB")
-})
+// mongoose.connection.once("open", () => {
+//     console.log("Conectado ao mongoDB")
+// })
 
-mongoose.connection?.on("error", (err) => {
-    console.error(`Error to connect - MongoDB: Error: ${err.message}`)
-})
+// mongoose.connection?.on("error", (err) => {
+//     console.error(`Error to connect - MongoDB: Error: ${err.message}`)
+// })
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
